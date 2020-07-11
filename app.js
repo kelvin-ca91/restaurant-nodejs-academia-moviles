a03f615ac68c76da3f3190b47de8e419f4d0091b
Kelvin Carrion Alfaro <kelvin.ca91@gmail.com>
2020-07-11 01:06:04 -0500

    avance de dishes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 const categoriesRoutes = require("./routes/categories");
+const dishesRouter = require("./routes/dishes");
 
 const mongoose = require("mongoose");
 
@@ -29,6 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/categories", categoriesRoutes);
+app.use("/dishes", dishesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
