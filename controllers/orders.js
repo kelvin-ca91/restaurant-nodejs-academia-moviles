e8e74578538d6d938a5eeb13d4450824107e4189
Kelvin Carrion Alfaro <kelvin.ca91@gmail.com>
2020-07-31 22:35:21 -0500

    sockets

--- controllers/orders.js
+++ controllers/orders.js
@@ -3,8 +3,25 @@ const dishesController = require("../controllers/dishes");
 
 const sockets = require("../sockets/base");
 const _ = require("lodash");
+const moment = require("moment");
+
+const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Setiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
 class Orders {
-  async create({ client_id, dishes }) {
+  async create({ client_id, dishes, date }) {
     try {
       const dishesIds = _.uniq(dishes.map((item) => item.dish_id));
       const dishesById = await dishesController.listByIds(dishesIds);
@@ -24,11 +41,62 @@ class Orders {
           dishes: newListDishes,
           cant_dishes: _.sumBy(dishes, "cant"),
           price_total: _.sumBy(newListDishes, "price"),
+          date: date ? date : undefined,
         };
-        sockets.newOrder(order);
-        return await orderModel.create(order);
+
+        await orderModel.create(order);
+        const lista = await this.list();
+        sockets.newOrder(lista);
+        await this.sendDataDashboard();
+        return order;
       }
       throw "ERROR";
+    } catch (error) {
+      throw "ERROR";
+    }
+  }
+
+  async sendDataDashboard() {
+    try {
+      const dataFinal = {
+        label: [],
+        data: [],
+      };
+
+      const lista = await this.list();
+      const listaNew = lista.map(
+        ({ date, _id, dishes, cant_dishes, price_total }) => {
+          return {
+            date,
+            _id,
+            dishes,
+            cant_dishes,
+            price_total,
+            onlyMonth: Number(moment(date).utc().format("M")),
+            onlyYear: moment(date).utc().format("YYYY"),
+          };
+        }
+      );
+      const groupByDate = _.groupBy(listaNew, "onlyYear");
+      months.forEach((month) => {
+        dataFinal.label.push(month);
+      });
+      for (const key in groupByDate) {
+        if (groupByDate.hasOwnProperty(key)) {
+          const data = [];
+          months.forEach((month, i) => {
+            data[i] = 0;
+            for (const item of groupByDate[key]) {
+              if (item.onlyMonth === i + 1) {
+                data[i] = item.price_total;
+              }
+            }
+          });
+          dataFinal.data.push({ label: key, data });
+        }
+      }
+      sockets.newOrderDashboard(dataFinal);
+      // return dataFinal;
     } catch (error) {
       throw "ERROR";
     }
--- sockets/base.js
+++ sockets/base.js
@@ -1,7 +1,8 @@
 let _io;
 // module.exports = (io) => {
-//   ios = io
+//   ios = io;
 //   io.on("connection", (socket) => {
+//     console.log("d");
 //     socket.on("new", (data) => {
 //       io.emit("notification", data);
 //     });
@@ -13,17 +14,24 @@ let _io;
 // };
 
 class Socket {
-  connect(io) {
-    io.on("connection", (socket) => {
-      // socket.on("new", (data) => {
-      //   io.emit("notification", data);
-      // });
+  async connect(io) {
+    const orderController = require("../controllers/orders");
+    io.on("connection", async (socket) => {
+      // console.log("conecto");
+      // console.log(socket);
+      socket.on("updateDashboard", async () => {
+        await orderController.sendDataDashboard();
+      });
     });
     _io = io;
   }
 
   newOrder(data) {
     _io.emit("newOrder", data);
+  }
+
+  newOrderDashboard(data) {
+    _io.emit("newOrderDashboard", data);
   }
 }
 
