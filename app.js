e544aa5926f313592bf0492f8a6068eb5e32aea3
Kelvin Carrion Alfaro <kelvin.ca91@gmail.com>
2020-09-08 00:01:12 -0500

    cors

--- app.js
+++ app.js
@@ -28,7 +28,18 @@ var corsOptions = {
   allowedHeaders: "Authorization, token, Content-Type",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
+// app.use(cors());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
